6bb7ca83e715932c30638991640ce6937fd3df06
Void <jomatahunde@gmail.com>
2026-05-25 16:50:32 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+ appId: 'com.joy.voidapp',
+  appName: 'Void app',
+  webDir: '.',
+  bundledWebRuntime: false
+};
+
+export default config;
